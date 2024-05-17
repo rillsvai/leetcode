0eee16b3707593e87a778b85d334c81fe6def45d
RillSvai <kirilkvas07@gmail.com>
2024-05-17 09:37:47 +0300

    feature: add 1038 or 538

--- src/lib.rs
+++ src/lib.rs
@@ -54,6 +54,7 @@ pub mod design {
 }
 
 pub mod tree {
+    pub mod binary_search_tree_to_greater_sum_tree;
     pub mod count_nodes_equal_to_average_of_subtree;
     pub mod deepes_leaves_sum;
     pub mod range_sum_of_bst;
---
+++ src/tree/binary_search_tree_to_greater_sum_tree.rs
@@ -0,0 +1,21 @@
+use std::{cell::RefCell, rc::Rc};
+
+use crate::common::tree_node::TreeNode;
+
+struct Solution {}
+
+impl Solution {
+    fn dfs(root: &Option<Rc<RefCell<TreeNode>>>, value: &mut i32) {
+        if let Some(node) = root {
+            let mut borrowed_node = node.borrow_mut();
+            Self::dfs(&borrowed_node.right, value);
+            *value += borrowed_node.val;
+            borrowed_node.val = *value;
+            Self::dfs(&borrowed_node.left, value);
+        }
+    }
+    pub fn bst_to_gst(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::dfs(&root, &mut 0);
+        root
+    }
+}
