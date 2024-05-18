10faa4fd875456b4c64ea2a11dfab7eed85bf2d1
RillSvai <kirilkvas07@gmail.com>
2024-05-18 10:00:59 +0300

    feature: add second solution for 1769

--- src/arrays/minimum_number_of_operations_to_move_all_balls_to_each_box.rs
+++ src/arrays/minimum_number_of_operations_to_move_all_balls_to_each_box.rs
@@ -1,3 +1,5 @@
+use std::os::fd::OwnedFd;
+
 pub fn min_operations(boxes: String) -> Vec<i32> {
     let mut result: Vec<i32> = vec![0; boxes.len()];
 
@@ -12,15 +14,51 @@ pub fn min_operations(boxes: String) -> Vec<i32> {
     result
 }
 
+pub fn min_operations2(boxes: String) -> Vec<i32> {
+    let mut result: Vec<i32> = vec![0; boxes.len()];
+
+    let mut operations_count = 0;
+    let mut non_empty_boxes_count = 0;
+    for (i, byte) in boxes.bytes().enumerate() {
+        result[i] += operations_count as i32;
+
+        non_empty_boxes_count += byte - 48;
+
+        operations_count += non_empty_boxes_count;
+    }
+
+    operations_count = 0;
+    non_empty_boxes_count = 0;
+    for (i, byte) in boxes.bytes().enumerate().rev() {
+        result[i] += operations_count as i32;
+
+        non_empty_boxes_count += byte - 48;
+
+        operations_count += non_empty_boxes_count;
+    }
+
+    result
+}
 #[cfg(test)]
 mod tests {
-    use crate::arrays::minimum_number_of_operations_to_move_all_balls_to_each_box::min_operations;
+    use crate::arrays::minimum_number_of_operations_to_move_all_balls_to_each_box::{
+        min_operations, min_operations2,
+    };
 
     #[test]
     fn test_min_operations() {
         assert_eq!(min_operations(String::from("110")), vec![1, 1, 3]);
         assert_eq!(
             min_operations(String::from("001011")),
+            vec![11, 8, 5, 4, 3, 4]
+        );
+    }
+
+    #[test]
+    fn test_min_operations2() {
+        assert_eq!(min_operations2(String::from("110")), vec![1, 1, 3]);
+        assert_eq!(
+            min_operations2(String::from("001011")),
             vec![11, 8, 5, 4, 3, 4]
         );
     }
