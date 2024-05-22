780fcfbccb4fe402d6594e9aa9f55b7a197c66a4
RillSvai <kirilkvas07@gmail.com>
2024-05-22 09:52:57 +0300

    update: update 2181;

--- src/linked_lists/merge_nodes_in_beetwen_zeros.rs
+++ src/linked_lists/merge_nodes_in_beetwen_zeros.rs
@@ -1,5 +1,26 @@
-pub fn merge_nodes(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {}
+use crate::common::list_node::ListNode;
 
+pub fn merge_nodes(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut res: Box<ListNode> = Box::new(ListNode::new(0));
+    let mut cur_node = &mut res;
+
+    let mut head = head.as_ref();
+
+    while let Some(node) = head {
+        head = node.next.as_ref();
+
+        if node.val == 0 && node.next != None {
+            cur_node.next = Some(Box::new(ListNode::new(0)));
+            cur_node = cur_node.next.as_mut().unwrap();
+
+            continue;
+        }
+
+        cur_node.val += node.val;
+    }
+
+    res.next
+}
 #[cfg(test)]
 mod tests {
     #[test]
