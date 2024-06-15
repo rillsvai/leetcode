97c44234411e7f5e87c17334421ac5105c1ca390
RillSvai <kirilkvas07@gmail.com>
2024-06-15 10:11:17 +0300

    feature: add 2885

---
+++ src/pandas/rename_columns.py
@@ -0,0 +1,3 @@
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    students.rename(columns={'id': 'student_id', 'first': 'first_name', 'last': 'last_name', 'age': 'age_in_years'}, inplace=True)
+    return students
